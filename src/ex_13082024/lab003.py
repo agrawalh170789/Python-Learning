79f1e8536e6ccacbcf197fc3b24ed614c776ecba
Himanshu Agrawal <agrawalh170789@gmail.com>
2024-08-16 21:27:21 +0530

    Homework Task1

---
+++ src/ex_13082024/lab003.py
@@ -0,0 +1,15 @@
+#Task1
+#Home Can you create a Program I will give you number(userinput and print table)
+#f"{}" String format concept
+#User input - float int -> 3.14 -> input
+userNum = float(input("Enter the number :"))
+print(f"{userNum}*1={userNum*1}")
+print(f"{userNum}*2={userNum*2}")
+print(f"{userNum}*3={userNum*3}")
+print(f"{userNum}*4={userNum*4}")
+print(f"{userNum}*5={userNum*5}")
+print(f"{userNum}*6={userNum*6}")
+print(f"{userNum}*7={userNum*7}")
+print(f"{userNum}*8={userNum*8}")
+print(f"{userNum}*9={userNum*9}")
+print(f"{userNum}*10={userNum*10}")
