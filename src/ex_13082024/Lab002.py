cc65d43a8d7d4d4deb1b46ffd239f142e681f7d6
Himanshu Agrawal <agrawalh170789@gmail.com>
2024-08-16 21:00:18 +0530

    Homework Task

---
+++ src/ex_13082024/Lab002.py
@@ -0,0 +1,19 @@
+"""""""""
+TASK2
+Create a program , take 2 inputs from the user num1, num2 and give them
+max
+pow num1 to num2
+sub, mul, sum, div.
+Format your out with f{""}
+"""""""""
+
+num1=int(input("Enter number 1: "))
+num2=int(input("Enter number 2: "))
+print("Number1:",num1)
+print("Number2:",num2)
+print("Maximum number between",num1,num2,"=",max(num1, num2))
+print("Power of",num1 ,num2,"=",pow(num1, num2))
+print("Substraction of",num1,num2,"=",(num1-num2))
+print("Addition of",num1,num2,"=",(num1+num2))
+print("Multiplication of",num1,num2,"=",(num1*num2))
+print("Divison of",num1,num2,"=",(num1/num2))
